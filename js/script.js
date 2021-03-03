8ae7fc3a677ae12a386a29229a431e28398a6b59
Dim <ultramaster20@gmail.com>
2021-03-03 13:01:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//let number = 4.6;
+//console.log(4/0);
+
+//const obj={
+//name: "John",
+//age: 25,
+//isMaried: false
+//};
+
+//console.log(obj.name);
+//console.log(obj["name"]);
+
+//alert("Hello!");
+//const result=confirm("Ar you here?");
+//console.log(result);
+
+//let answer = prompt("Вам есть 18?", "");
+//console.log(answer+5);
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt('Ваше имя', ' ');
+//answers[1] = prompt('Ваша фамилия', ' ');
+//answers[2] = prompt('Ваш возраст', ' ');
+//document.write(answers);
+
+//const namen = 'Dmitry';
+//alert(`Привет,${namen}`);
+
+let incr = 10,
+	 decr = 10;
+
+//	 incr++;
+//	 decr--;
+	// console.log(incr);
+	// console.log(decr);
+	// console.log(incr++);
+	// console.log(decr--);
+	 console.log(++incr);
+	 console.log(--decr);
+	 
+	 console.log(5%2);
+
