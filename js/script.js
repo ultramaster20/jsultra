22e00feaeec851a2c570f0567ef191ee71de42f5
Dim <ultramaster20@gmail.com>
2021-03-04 15:55:09 +0300

    callback

--- js/script.js
+++ js/script.js
@@ -1,103 +1,10 @@
 'use strict';
 
-let numberOfFilms;
-
-function start () {
-	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-	while (numberOfFilms == '' || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-	}
+function lernJS (lang,callback) {
+	console.log(`Я учу ${lang}`);
+	callback();
 }
-start ();
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-function rememberMyFilms() {
-	for(let i = 0; i < 2; i++) {
-		const a = prompt("Один из последних просмотренных фильмов?", " "),
-				b = prompt("На сколько оцените его?", " ");
-		
-				if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-					personalMovieDB.movies[a] = b;
-					console.log('done');
-				} else {
-					console.log('error');
-					i--;
-				}		
-		}
-}
-rememberMyFilms();
-
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log("Просмотрено довольно мало фильмов");
-		} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-			console.log("Вы классический зритель");
-		} else if (personalMovieDB.count > 30) {
-			console.log("Вы киноман");
-		} else {
-			console.log("Произошла ошибка");
-		}
-}
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-	if (!hidden) {
-console.log(personalMovieDB);
-	}
-}
-showMyDB (personalMovieDB.privat);
-
-function writeYourGenres () {
-	for (let i = 1; i <= 3; i++){
-		const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-		personalMovieDB.genres[i-1] = genre;
-	}
-}
-writeYourGenres ();
-
-
-
-
-
-
-
-//let num = 0;
-//while (num < 2) {
-//const a = prompt("Один из последних просмотренных фильмов?", " "),
-//		b = prompt("На сколько оцените его?", " ");
-//		num++;
-
-//		if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-//			personalMovieDB.movies[a] = b;
-//			console.log('done');
-//		} else {
-//			console.log('error');
-//			i--;
-//		}		
-//}
-
-//let num =0;
-//do {
-//const a = prompt("Один из последних просмотренных фильмов?", " "),
-//		b = prompt("На сколько оцените его?", " ");
-//		num++;
-
-//		if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-//			personalMovieDB.movies[a] = b;
-//			console.log('done');
-//		} else {
-//			console.log('error');
-//			i--;
-//		}		
-//}
-//while (num < 2);
-
-
-
+	lernJS ('JavaScript', function (){
+		console.log('Я прошёл этот урок');
+	});
