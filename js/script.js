6aec33cb66d599df2243e7fdb025401b9458d06b
Dim <ultramaster20@gmail.com>
2021-03-04 18:46:39 +0300

    object_svoistva

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 'use strict';
 
-function lernJS (lang,callback) {
-	console.log(`Я учу ${lang}`);
-	callback();
+const options ={
+	a: "b",
+	c: 8,
+	d: {
+		e: "f",
+		h: 9,
+	},
+	makeTest: function () {
+		console.log("TEST");
+	}
+};
+
+options.makeTest();
+const {e, f} = options.d;
+console.log(e);
+
+console.log(Object.keys(options).length);
+
+
+
+let counter =0;
+
+for (let key in options) {
+	if (typeof(options[key]) === 'object') {
+		for (let i in options[key]) {
+		console.log (`Свойство ${i} имеет значение ${options[key][i]}`);
+		counter++;
+	} 
+} else{
+		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+		counter++;
+	}
 }
 
-	lernJS ('JavaScript', function (){
-		console.log('Я прошёл этот урок');
-	});
+console.log(counter);
